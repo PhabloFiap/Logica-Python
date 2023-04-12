30de2fc1b47f64b8671e28a4c3e465b6e3565ea3
PhabloFiap <rm550687@fiap.com.br>
2023-04-11 21:59:01 -0300

    Exercicio de Palindrome

---
+++ Ex12Lista4.py
@@ -0,0 +1,17 @@
+num = int (input ("Informe um numer: "))
+num_copia = num
+invertido = ""
+
+
+
+while num!= 0:
+    digito = num % 10
+    invertido = invertido + str (digito)
+    num = num //10
+
+
+print(invertido)
+if int(invertido) == num_copia:
+    print("É palindrome")
+else:
+    print("Não é palindrome")
