ec92deb57dd6bdf798d64ca2cdc12e37d1a08bc9
PhabloFiap <rm550687@fiap.com.br>
2023-04-27 18:18:38 -0300

    Refatorando codigo de sequencia de numeros

--- Ex3Lista4.py
+++ Ex3Lista4.py
@@ -7,10 +7,10 @@ while n>0:
     sequencia = int (input("Digite o numero da sequencia"))
     n = n -1
     if sequencia>0:
-        contaNumeroPositivo = contaNumeroPositivo +1
+        contaNumeroPositivo +=1
 
     else:
-        contaNumeroNegativo = contaNumeroNegativo +1
+        contaNumeroNegativo +=1
 
 print ("Numeros positivos: "+  str (contaNumeroPositivo))
 print ("Numeros negativos: " + str (contaNumeroNegativo))
