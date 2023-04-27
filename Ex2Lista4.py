b9d715ed9e8a690130ebee76748a6aaaf8fa52b5
PhabloFiap <rm550687@fiap.com.br>
2023-04-27 17:36:21 -0300

    media do aluno indicando <5 >5

--- Ex2Lista4.py
+++ Ex2Lista4.py
@@ -2,9 +2,18 @@ n = int (input("qtd de alunos: "))
 
 contador = 0
 soma = 0
-while contador <n:
-    nota = float(input ("nota do aluno: "))
+notamenor = 0
+notamaior = 0
+while n > contador:
+    nota = int (input("Digite a nota do aluno"))
     soma = soma + nota
-    contador = contador +1
+    contador = contador + 1
+    if nota <6:
+        notamenor = notamenor+ 1
+    else:
+        notamaior= notamaior +1
 
-print (f"a soma vale:  {soma/n}")
+media = soma / n
+print(media)
+print(notamenor)
+print(notamaior)
