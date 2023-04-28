1b4273d2ef81e4c856523348fddb62bafa882cbb
PhabloFiap <rm550687@fiap.com.br>
2023-04-27 21:20:50 -0300

    codigo refatorado + verificador se num é primo

--- Ex5Lista3.py
+++ Ex5Lista3.py
@@ -1,9 +1,18 @@
 n = int (input("Digite um numero: "))
 n2 = n
 divisor =0
+primo =0
 
 while n>0:
+
     if n2%n ==0:
         divisor += 1
+
+    
+
     n-=1
+if divisor==2 or n ==1:
+        print(str (n2) +" é primo")
+
 print (divisor)
+print (primo)
